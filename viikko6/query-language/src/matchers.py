74e450796704d7286045fbf17f152bf8f40b9fdd
hhuuskon <henri.huuskonen@outlook.com>
2023-12-11 12:30:12 +0200

    viikko6 tehtava4 palautus

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -66,3 +66,21 @@ class Or:
                 return True
 
         return False
+    
+# https://ohjelmistotuotanto-hy.github.io/osa4/#pinorakentaja-viikko-6
+
+class QueryBuilder:
+    def __init__(self, pino = All()):
+        self._pino = pino
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self._pino, PlaysIn(team)))
+    
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self._pino, HasAtLeast(value, attr)))
+    
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self._pino, HasFewerThan(value, attr)))
+    
+    def build(self):
+        return self._pino
